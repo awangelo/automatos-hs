eabdafc82ea02d0816c21292d404627a0f802cc2
awangelo <anggg.gaio@gmail.com>
2025-08-22 15:26:22 -0300

    add exercicios l1 e l4

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,22 @@
 module Main (main) where
 
-import DFAs (automato_L2, automato_L3)
+import DFAs (automato_L1, automato_L2, automato_L3, automato_L4)
 import Repl (repl)
 import System.IO (hFlush, stdout)
 
 main :: IO ()
 main = do
     putStrLn "Escolha um automato"
-    putStrLn "1. L2 = { w | |w| > 3 }"
-    putStrLn "2. L3 = { w | w tem prefixo 'aa' }"
+    putStrLn "1. L1 = { w | aa ∨ bb sao subpalavras de w }"
+    putStrLn "2. L2 = { w | |w| > 3 }"
+    putStrLn "3. L3 = { w | w tem prefixo 'aa' }"
+    putStrLn "4. L4 = { w | |w| eh multiplo de 3 }"
     putStr "> "
     hFlush stdout
     opcao <- getLine
     case opcao of
-        "1" -> repl automato_L2
-        "2" -> repl automato_L3
+        "1" -> repl automato_L1
+        "2" -> repl automato_L2
+        "3" -> repl automato_L3
+        "4" -> repl automato_L4
         _ -> putStrLn "?"
--- src/DFAs.hs
+++ src/DFAs.hs
@@ -1,4 +1,4 @@
-module DFAs (automato_L2, automato_L3) where
+module DFAs (automato_L1, automato_L2, automato_L3, automato_L4) where
 
 import Types (
     Automato,
@@ -9,6 +9,26 @@ import Types (
     mkEstadosFinais,
     mkTransicao,
  )
+
+-- L1 = { w ⋹ {a, b}* | "aa" ∨ "bb" ⊆ w }
+automato_L1 :: Automato String
+automato_L1 =
+    mkAutomato
+        (mkAlfabeto ['a', 'b'])
+        (mkEstados ["q_0", "q_1", "q_2", "q_3"])
+        (mkTransicao transicao_L1)
+        (mkEstadoInicial "q_0")
+        (mkEstadosFinais ["q_3"])
+  where
+    transicao_L1 ("q_0", 'a') = "q_1"
+    transicao_L1 ("q_0", 'b') = "q_2"
+    transicao_L1 ("q_1", 'a') = "q_3"
+    transicao_L1 ("q_1", 'b') = "q_2"
+    transicao_L1 ("q_2", 'a') = "q_1"
+    transicao_L1 ("q_2", 'b') = "q_3"
+    transicao_L1 ("q_3", 'a') = "q_3"
+    transicao_L1 ("q_3", 'b') = "q_3"
+    transicao_L1 _ = error "Transicao invalida."
 
 -- L2 = { w ∈ {a,b}* | |w| > 3 }
 automato_L2 :: Automato String
@@ -44,3 +64,21 @@ automato_L3 =
     transicao_L3 ("q_2", _) = "q_2" -- uma vez confirmado prefixo, sempre aceita
     transicao_L3 ("q_reject", _) = "q_reject" -- estado de rejeição (trap state)
     transicao_L3 _ = error "Transicao invalida."
+
+-- L4 = { w ∈ {a,b}* | |w| eh multiplo de 3 }
+automato_L4 :: Automato String
+automato_L4 =
+    mkAutomato
+        (mkAlfabeto ['a', 'b'])
+        (mkEstados ["q_0", "q_1", "q_2", "q_3", "q_4"])
+        (mkTransicao transicao_L4)
+        (mkEstadoInicial "q_0")
+        (mkEstadosFinais ["q_0"])
+  where
+    transicao_L4 ("q_0", _) = "q_1"
+    transicao_L4 ("q_1", _) = "q_2"
+    transicao_L4 ("q_2", _) = "q_0"
+    transicao_L4 ("q_0", _) = "q_0"
+    transicao_L4 _ = error "Transicao invalida."
+
+
